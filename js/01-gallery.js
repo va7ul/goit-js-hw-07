7372e31a6bae585017e1c2c17de3f214b387b5cf
va7ul <vas7ul@gmail.com>
2023-06-12 18:53:14 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,14 +7,14 @@ const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
 
 function createGalleryMarkup(items) {
-    return items.map(item => 
+    return items.map(({preview, original, description}) => 
         `<li class="gallery__item">
-            <a class="gallery__link" href="${item.original}">
+            <a class="gallery__link" href="${original}">
                 <img
                     class="gallery__image"
-                    src="${item.preview}"
-                    data-source="${item.original}"
-                    alt="${item.description}"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
                 />
             </a>
         </li>`)
