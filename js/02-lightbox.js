e3068dd16b66afc3258add75474795630c60bcff
va7ul <vas7ul@gmail.com>
2023-06-12 18:53:27 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+function createGalleryMarkup(items) {
+    return items.map(({preview, original, description}) => 
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`)
+    .join('');
+};
+
+imageFullscreenOpener();
+
+function imageFullscreenOpener() {
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+}
+
 console.log(galleryItems);
